8b9fcc0fde7d371aa9010243c922760791ba1375
lauralubis37-max <lubislaura08@gmail.com>
2025-11-21 09:51:03 +0700

    Pair-27
    memperbaiki

--- TLAST.java
+++ TLAST.java
@@ -1,7 +1,10 @@
+// 12S25027 - Jessica Mariani Situmorang
+// 12S25037 - Laura Lubis
+
 import java.util.*;
 import java.lang.Math;
 
-public class JavaApplication {
+public class TLAST {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
